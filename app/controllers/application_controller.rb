0d0d17e0e94d53ae5905ed3a6cebbc1dd4f2095b
Karin Fernandez <karinfdez@yahoo.com>
2016-03-24 13:55:55 -0400

    Added 3 more headers to app_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,11 @@ def add_allow_credentials_headers
   # (using 'withCredentials' in the XMLHttpRequest), we need to add some headers so                                                                                                                                                                                                      
   # the browser will not reject the response                                                                                                                                                                                                                                             
   response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'                                                                                                                                                                                                     
-  response.headers['Access-Control-Allow-Credentials'] = 'true'                                                                                                                                                                                                                          
+  response.headers['Access-Control-Allow-Credentials'] = 'true'     
+  response.headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+  headers['Access-Control-Request-Method'] = '*'
+  headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+                                                                                                                                                                                                                     
 end 
 
 def options                                                                                                                                                                                                                                                                              
